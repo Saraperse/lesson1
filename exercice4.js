0fd5b1a0f979d9ad7a6c21cb7ee3287d9c3bf4a9
saraperse <srh.rouhani@gmail.com>
2021-05-04 16:29:09 +0200

    nom,âge, sexe

---
+++ exercice4.js
@@ -0,0 +1,15 @@
+var a = "Galon";
+var b = 18;
+var c = true;
+
+console.log("Bonjour "  +  a );
+if( b < 18){
+    console.log("vous êtes mineur(e)");
+}else {
+    console.log("vous êtes majeur(e)");
+}
+if (c = true){
+    console.log("Vous êtes un homme");
+}else{
+    console.log("Vous êtes une femme");
+}
